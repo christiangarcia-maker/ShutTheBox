ddbd6a5dabf3fa789ee64fcdc6b99dc0ebd54f89
miguelcabello <miguel.cabello@iesnervion.es>
2026-01-26 10:36:15 +0100

    Merge branch 'master' of https://github.com/christiangarcia-maker/ShutTheBox.git

--- src/shutthebox/Functions.java
+++ src/shutthebox/Functions.java
@@ -21,12 +21,27 @@ public class Functions {
 		
 	}
 	
-	public void checkTiles(int[] player, int throwTotal) {
-		
+	public void checkTiles(int[] player, int throwTotal) {		
+		boolean possibleMove = false;
+		for (int i = 0; i < player.length; i++) {
+			for (int j = 0; j < player.length; j++) {
+				if (player[i] != player[j] && player[i] + player[j] == throwTotal) {
+					possibleMove = true;
+				}
+			}
+		}
+		if (possibleMove) {
+			System.out.println("You can make a move!");
+			select(player, throwTotal);
+		} else {
+			System.out.println("You can't make a move!");
+			endTurn(player);
+		}
+	}
+	
+	public void select(int[] player, int throwTotal) {
 		System.out.print("You missing the tiles: ");
-
 		boolean firstTile = true;
-		
 		for (int i = 0; i < player.length; i++) {
 			if (player[i] != 0) {
 				if (firstTile) {
@@ -38,29 +53,25 @@ public class Functions {
 			}
 		}
 		
-		try {
-			boolean correctValue = false;
-			int election = 0;
-			do {
+		boolean correctValue = false;
+		int election = 0;
+		do {
+			try {
 				try {
-					try {
-						System.out.println("¿Do you want to use just the number or a combination? (1/2): ");
-						election = sc.nextInt();
-						assert (election>0&&election<=2);
-						correctValue = true;
-					} catch (AssertionError e) {
-						System.out.println("Incorrect input, try again");
-					}
-				} catch (InputMismatchException e) {
+					System.out.println("¿Do you want to use just the number or a combination? (1/2): ");
+					election = sc.nextInt();
+					assert (election>0&&election<=2);
+					correctValue = true;
+				} catch (AssertionError e) {
 					System.out.println("Incorrect input, try again");
 				}
-			} while (!correctValue);
-			
-			
-			
-		} catch (AssertionError e){
-			
-		}
+			} catch (InputMismatchException e) {
+				System.out.println("Incorrect input, try again");
+			}
+		} while (!correctValue);
+	}
+	
+	public void endTurn(int[] player) {
 		
 	}
 
