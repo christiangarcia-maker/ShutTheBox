a37a9f7630a91a286164375e149621c00ef3b01f
fatima.roman <fatima.roman@106-12.info2>
2026-01-26 09:47:08 +0100

    n

--- src/shutthebox/Main.java
+++ src/shutthebox/Main.java
@@ -1,8 +1,28 @@
 package shutthebox;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean playerGame = true; 
+		
+		while (playerGame) {
+			
+		}
+		
+		
+		sc.close();
+		
+	}
+	
+	public static void playerEnd(int[] player, boolean playerGame) {
+		while (playerGame) {
+			
+		}
 		
 	}
 
