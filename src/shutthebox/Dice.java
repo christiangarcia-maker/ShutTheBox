6edc96c6b6a0674c50b0461a48fdf5606bc1ad17
Rocio <qr.ye@iesnervion.es>
2026-03-08 18:31:00 +0100

    Refactor Dice class comments and documentation
    Updated comments for clarity and improved code documentation.

--- src/shutthebox/Dice.java
+++ src/shutthebox/Dice.java
@@ -1,15 +1,20 @@
 package shutthebox;
 
 public class Dice {
-	
+
+	// Private variable that stores the current value of the die (visible face)
+	// Initializes to 1 by default
     private int value = 1;  // default 1
     /*
-     * Explicación ¿?
-     * Al crear un objeto dado necesitariamos aletarorio el indice (Dice = {1,2,3,4,5,6})
-     * Entonces creamos un objeto dado que directamente almacene un valor aleatorio 
+	 * Explication: 
+     * When creating a given object, we need to randomize the index (Dice = {1,2,3,4,5,6}). 
+	 * Therefore, we create a given object that directly stores a random value.
      */
+	
     // Roll this dice (1-6) and update value
     public int roll() {
+		// Uses the static random number generator from the Main class
+		// "nextInt(1,7)" generates numbers from 1 to 6 (the upper limit is exclusive)
         this.value = Main.random.nextInt(1,7);
         return value;
     }
@@ -25,17 +30,24 @@ public class Dice {
 	
     // Roll two dice and return sum
     public static int rollTwo(Dice dice1, Dice dice2) {
-        dice1.roll();
+        // Throw the first die
+		dice1.roll(); 
+		// Throw the second die
         dice2.roll();
+		// Return the sum of the values obtained 
         return dice1.getValue() + dice2.getValue();
     }
 
     // Roll two dice and return individual values for display
     public static int[] rollTwoValues(Dice dice1, Dice dice2) {
+		// Throw the first die
         dice1.roll();
+		// Throw the second die
         dice2.roll();
+		// Creates and returns an array with the two values
         return new int[]{dice1.getValue(), dice2.getValue()};
     }
 	
 	
 }
+
